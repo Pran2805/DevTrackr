bb37f66be32571c2e71744b7e34b456389b38102
Pran2805 <pranavshinde.as@gmail.com>
2026-03-17 10:45:54 +0530

    Project Hard Reset

--- client/src/pages/LoadingPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react'
-
-export default function LoadingPage() {
-  return (
-    <div className='h-screen flex items-center justify-center'>
-      <Loader2 className='animate-spin'/>
-    </div>
-  )
-}
--- server/src/db/index.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-import ENV from '../utils/env.ts'
-
-export const connectDB = async() =>{
-    const connection = await mongoose.connect(ENV.dbUrl)
-    console.log("Database connected successfully", connection.connection.host)
-}
